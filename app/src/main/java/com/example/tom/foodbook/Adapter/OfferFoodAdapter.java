d360f85657a135388e007a145a25e2735a8b79c7
BartosStore <e-miro@atlas.cz>
2016-07-29 09:45:16 +0800

    Created Adapter for food list

---
+++ app/src/main/java/com/example/tom/foodbook/Adapter/OfferFoodAdapter.java
@@ -0,0 +1,18 @@
+package com.example.tom.foodbook.Adapter;
+
+import com.example.tom.foodbook.Food;
+
+import java.util.List;
+
+/**
+ * Created by Mira on 29.7.2016.
+ */
+public class OfferFoodAdapter {
+
+    private List<Food> foodList;
+
+    public OfferFoodAdapter(List<Food> foodList) {
+        this.foodList = foodList;
+    }
+
+}
