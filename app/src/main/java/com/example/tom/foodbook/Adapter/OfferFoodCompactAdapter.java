7ed2465d49004e133e50edafddec7e8de4d4896e
Tomáš <tomas.archalous@gmail.com>
2016-08-01 22:24:57 +0800

    Canteen overview with list of food

---
+++ app/src/main/java/com/example/tom/foodbook/Adapter/OfferFoodCompactAdapter.java
@@ -0,0 +1,99 @@
+package com.example.tom.foodbook.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import com.example.tom.foodbook.Entity.Food;
+import com.example.tom.foodbook.R;
+
+import java.util.List;
+
+/**
+ * Created by Mira on 29.7.2016.
+ */
+public class OfferFoodCompactAdapter extends RecyclerView.Adapter<OfferFoodCompactAdapter.ViewHolder> {
+
+    public static final String MIBA_TAG = "MIBA_TAG";
+
+    private ViewHolder.ClickListener clickListener;
+    private List<Food> foodList;
+    Context canteensListActivityContext;
+
+    // inicialization of adapter
+    // pass food data through Constructor on Activity load
+    public OfferFoodCompactAdapter(List<Food> foodList, Context canteensListActivityContext, ViewHolder.ClickListener clickListener) {
+        this.foodList = foodList;
+        this.clickListener = clickListener;
+        this.canteensListActivityContext = canteensListActivityContext;
+    }
+
+    public void setData(List<Food> foodList) {
+        Log.d(MIBA_TAG, "OfferFoodAdapter => setData: " + foodList);
+        this.foodList.clear();
+        this.foodList.addAll(foodList);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public OfferFoodCompactAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_offer_food_compact, parent, false);
+        return new ViewHolder(itemView, clickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Food food = foodList.get(position);
+
+   holder.name.setText(food.getName());
+//        holder.calories.setText(food.getCalories() + " kCal");
+//        holder.price.setText("Price: " + food.getPrice() + " HKD");
+//        holder.proteins.setText("Proteins: " + food.getProteins() + " g");
+//        holder.fat.setText("Fats: " + food.getFats() + " g");
+//        holder.sugar.setText("Sugar: " + food.getSugar() + " g");
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodList.size();
+    }
+
+    public List<Food> getFoodList() {
+        return foodList;
+    }
+
+    // representation of one row of RecycleView
+    public static class ViewHolder extends RecyclerView.ViewHolder implements AdapterView.OnClickListener {
+
+        private ViewHolder.ClickListener clickListener;
+        private TextView name, price, calories, fat, sugar, proteins;
+
+        public ViewHolder(View itemView, ClickListener listener) {
+            super(itemView);
+           name = (TextView) itemView.findViewById(R.id.tv_food_name);
+//            price = (TextView) itemView.findViewById(R.id.tv_food_price);
+//            calories = (TextView) itemView.findViewById(R.id.tv_food_calories);
+//            proteins = (TextView) itemView.findViewById(R.id.tv_food_proteins);
+//            fat = (TextView) itemView.findViewById(R.id.tv_food_fats);
+//            sugar = (TextView) itemView.findViewById(R.id.tv_food_sugar);
+
+            clickListener = listener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            clickListener.onItemClick(getAdapterPosition());
+        }
+
+        public interface ClickListener {
+            void onItemClick(int position);
+        }
+    }
+
+}
--- app/src/main/java/com/example/tom/foodbook/CanteenOverviewActivity.java
+++ app/src/main/java/com/example/tom/foodbook/CanteenOverviewActivity.java
@@ -12,14 +12,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tom.foodbook.Adapter.OfferCanteenAdapter;
+import com.example.tom.foodbook.Adapter.OfferFoodAdapter;
+import com.example.tom.foodbook.Adapter.OfferFoodCompactAdapter;
 import com.example.tom.foodbook.Entity.Canteen;
+import com.example.tom.foodbook.Entity.Food;
 
 import java.util.ArrayList;
 
-public class CanteenOverviewActivity extends AppCompatActivity {
+public class CanteenOverviewActivity extends AppCompatActivity implements OfferFoodCompactAdapter.ViewHolder.ClickListener {
 
     private Canteen canteen;
     private CsvHelper csvHelper;
+
+    public static final String MIBA_TAG = "MIBA_TAG";
+
+    private ArrayList<Food> offerFoodList;
+    private RecyclerView rvOfferFood;
+    private RecyclerView.LayoutManager offerFoodLayoutManager;
+    private OfferFoodCompactAdapter offerFoodAdapter;
+
 
 
     @Override
@@ -49,6 +60,30 @@ public class CanteenOverviewActivity extends AppCompatActivity {
 
         img_canteen.setImageResource(canteen.getImageResource(CanteenOverviewActivity.this));
 
+
+
+                /* ************************************************************************************** */
+
+        //RecyclerView.ItemDecoration itemDecoration
+        rvOfferFood = (RecyclerView) findViewById(R.id.rv_offer_canteen);
+        rvOfferFood.setHasFixedSize(true);
+        offerFoodLayoutManager = new LinearLayoutManager(this);
+        rvOfferFood.setLayoutManager(offerFoodLayoutManager);
+
+        /* ************************************************************************************** */
+
+        CsvHelper csvHelper = new CsvHelper(CanteenOverviewActivity.this);
+        this.offerFoodList = csvHelper.getFoods();
+
+        offerFoodAdapter = new OfferFoodCompactAdapter(offerFoodList, CanteenOverviewActivity.this, this);
+        rvOfferFood.setAdapter(offerFoodAdapter);
+
+
+
     }
 
+    @Override
+    public void onItemClick(int position) {
+
+    }
 }
